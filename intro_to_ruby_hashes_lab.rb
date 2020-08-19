91c92f17f8b0312d51009abe42029156e2e8bdfc
Alma Flores <flores.almalee@gmail.com>
2020-08-19 21:48:17 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -51,7 +51,7 @@ def monopoly_with_fourth_tier
 	    :b_and_o_railroad => {'mortgage_value' => '$400'},
 	    :shortline => {'mortgage_value' => '$800'}
 	  },
-	  :rent_in_dollar => {
+	  :rent_in_dollars => {
 	    :one_piece_owned => 25,
 	    :two_pieces_owned => 50,
 	    :three_pieces_owned => 100,
